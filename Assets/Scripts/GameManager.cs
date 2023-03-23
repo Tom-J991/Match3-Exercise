892fcd0fee168584d0c5322bb5112e94683067af
AIE\s210991 <s210991@students.aie.edu.au>
2023-03-23 11:43:54 +1030

    Improved board generation Implemented tile swapping Implemented match checking Implemented board refill

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,83 +1,137 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
-
-public enum GemType
-{
-    Purple = 0,
-    Blue,
-    Green,
-    Pink,
-    Red,
-}
-
-public class Gem
-{
-    private GameObject _gemObject;
-    private GemType _type;
-
-    public Gem(GemType type, Sprite[] spriteList, Transform parent, Vector2 position)
-    {
-        _type = type;
-        _gemObject = new GameObject("Gem");
-        _gemObject.transform.SetParent(parent); // Set parent to grid object.
-        _gemObject.transform.localScale = Vector3.one / 8.0f;
-        _gemObject.transform.position += parent.position; // Ignore parent offset.
-        _gemObject.transform.position += new Vector3(position.x, position.y, 0.0f);
-
-        _gemObject.AddComponent<SpriteRenderer>();
-        _gemObject.GetComponent<SpriteRenderer>().sprite = spriteList[(int)_type];
-    }
-
-    // Setters and Getters
-    public GemType Type
-    {
-        get
-        {
-            return _type;
-        }
-    }
-    public int IType
-    {
-        get
-        {
-            return (int)_type;
-        }
-    }
-}
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private int _gridWidth;
     [SerializeField] private int _gridHeight;
-    [SerializeField] private Sprite[] _sprites;
+    [SerializeField] private GameObject gemPrefab;
+
+    public Sprite[] sprites;
 
     private GameObject _gridObject;
-    private List<Gem> _grid;
+    private GameObject[,] _gems;
+
+    [HideInInspector] public bool _shifting { get; set; }
 
     void Start()
     {
-        _grid = new List<Gem>(); // Grid Array
+        GameManager instance = transform.GetComponent<GameManager>();
+
+        _gems = new GameObject[_gridWidth, _gridHeight];
+
         _gridObject = new GameObject("Grid"); // Grid Object
         _gridObject.transform.position = new Vector3(-((_gridWidth-1.0f)/2.0f), -((_gridHeight-1.0f)/2.0f), 0.0f); // Offset to center.
 
         // Create grid of gems.
         int type = 0;
+        int[] lType = new int[_gridHeight]; // to left
+        int dType = 0; // below
         for (int i = 0; i < _gridWidth; ++i)
         {
             for (int j = 0; j < _gridHeight; ++j)
             {
-                type = Random.Range(0, _sprites.Length);
+                // Get surrounding gems and find non-repeating type.
+                List<int> possibleGems = new List<int>();
+                possibleGems.AddRange(Enum.GetValues(typeof(GemType)).Cast<int>());
+                possibleGems.Remove(lType[j]);
+                possibleGems.Remove(dType);
+                type = possibleGems[UnityEngine.Random.Range(0, possibleGems.Count-1)];
                 // Add Gem
-                Gem gem = new Gem((GemType)type, _sprites, _gridObject.transform, new Vector2(i, j));
-                _grid.Add(gem); // Add to array
+                _gems[i, j] = CreateGem(_gridObject.transform, (GemType)type, new Vector2(i, j));
+                //
+                lType[j] = type;
+                dType = type;
             }
         }
     }
     void Update()
     {
         
+    }
+
+    GameObject CreateGem(Transform parent, GemType type, Vector2 position)
+    {
+        GameObject gemObject = Instantiate(gemPrefab, parent);
+        gemObject.transform.localScale = Vector3.one / 6.0f;
+        gemObject.transform.position += new Vector3(position.x, position.y, 0.0f);
+        gemObject.name = "Gem { " + position.x + ", " + position.y + " }";
+
+        Gem gem = gemObject.GetComponent<Gem>();
+        gem.gameInstance = transform.GetComponent<GameManager>();
+        gem.type = type;
+
+        gemObject.GetComponent<SpriteRenderer>().sprite = sprites[(int)type];
+
+        return gemObject;
+    }
+
+    public IEnumerator FindNullTiles()
+    {
+        for (int x = 0; x < _gridWidth; x++)
+        {
+            for (int y = 0; y < _gridHeight; y++)
+            {
+                if (_gems[x, y].GetComponent<SpriteRenderer>().sprite == null || _gems[x, y].GetComponent<Gem>().type == GemType.NONE)
+                {
+                    yield return StartCoroutine(ShiftTilesDown(x, y));
+                    break;
+                }
+            }
+        }
+
+        for (int x = 0; x < _gridWidth; x++)
+        {
+            for (int y = 0; y < _gridHeight; y++)
+            {
+                _gems[x, y].GetComponent<Gem>().ClearAllMatches();
+            }
+        }
+    }
+    private IEnumerator ShiftTilesDown(int _x, int _y, float shiftDelay = 0.03f)
+    {
+        _shifting = true;
+        List<GameObject> gemsToShift = new List<GameObject>();
+        int nullCount = 0;
+
+        for (int y = _y; y < _gridHeight; y++)
+        {
+            if (_gems[_x, y].GetComponent<SpriteRenderer>().sprite == null || _gems[_x, y].GetComponent<Gem>().type == GemType.NONE)
+                nullCount++;
+            gemsToShift.Add(_gems[_x, y]);
+        }
+
+        for (int i = 0; i < nullCount; i++)
+        {
+            yield return new WaitForSeconds(shiftDelay);
+            for (int j = 0; j < gemsToShift.Count-1; j++)
+            {
+                gemsToShift[j].GetComponent<SpriteRenderer>().sprite = gemsToShift[j+1].GetComponent<SpriteRenderer>().sprite;
+                gemsToShift[j].GetComponent<Gem>().type = gemsToShift[j+1].GetComponent<Gem>().type;
+
+                int newType = GetNewGem(_x, _gridHeight-1);
+                gemsToShift[j+1].GetComponent<Gem>().type = (GemType)newType;
+                gemsToShift[j+1].GetComponent<SpriteRenderer>().sprite = sprites[(int)newType];
+            }
+        }
+
+        _shifting = false;
+    }
+    private int GetNewGem(int x, int y)
+    {
+        List<int> possibleGems = new List<int>();
+        possibleGems.AddRange(Enum.GetValues(typeof(GemType)).Cast<int>());
+        if (x > 0)
+            possibleGems.Remove(_gems[x-1, y].GetComponent<Gem>().IType);
+        if (x < _gridWidth - 1)
+            possibleGems.Remove(_gems[x+1, y].GetComponent<Gem>().IType);
+        if (y > 0)
+            possibleGems.Remove(_gems[x, y-1].GetComponent<Gem>().IType);
+
+        return possibleGems[UnityEngine.Random.Range(0, possibleGems.Count-1)];
     }
 }
---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,177 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public enum GemType
+{
+    Purple = 0,
+    Blue,
+    Green,
+    Pink,
+    Red,
+    NONE = -1
+}
+
+public class Gem : MonoBehaviour, IPointerDownHandler
+{
+    [HideInInspector] public GameManager gameInstance;
+    [HideInInspector] public GemType type;
+
+    static private Gem _prevSelected;
+    private bool _isSelected;
+
+    private bool _matchFound;
+
+    private Vector2[] _adjacentDirs = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+
+    public void Start()
+    {
+
+    }
+    public void Update()
+    {
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        if (transform.GetComponent<SpriteRenderer>().sprite == null || type == GemType.NONE || gameInstance._shifting)
+            return;
+
+        if (_isSelected)
+            Deselect();
+        else
+        {
+            if (_prevSelected == null)
+                Select();
+            else
+            {
+                if (GetAllAdjacentGems().Contains(_prevSelected.gameObject))
+                {
+                    SwapSprite(_prevSelected);
+                    _prevSelected.ClearAllMatches();
+                    _prevSelected.Deselect();
+                    ClearAllMatches();
+                }
+                else
+                {
+                    _prevSelected.Deselect();
+                    Select();
+                }
+            }
+        }
+    }
+
+    private void Select()
+    {
+        transform.GetComponent<SpriteRenderer>().color = Color.gray;
+        _isSelected = true;
+        _prevSelected = transform.GetComponent<Gem>();
+    }
+    private void Deselect()
+    {
+        transform.GetComponent<SpriteRenderer>().color = Color.white;
+        _isSelected = false;
+        _prevSelected = null;
+    }
+
+    private void SwapSprite(Gem other)
+    {
+        if (type == other.Type)
+            return;
+
+        GemType tmpType = other.Type;
+        other.Type = type;
+        type = tmpType;
+
+        transform.GetComponent<SpriteRenderer>().sprite = gameInstance.sprites[(int)type];
+        other.GetComponent<SpriteRenderer>().sprite = gameInstance.sprites[(int)other.Type];
+    }
+
+    private GameObject GetAdjacent(Vector2 dir)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir);
+        if (hit.collider != null)
+        {
+            Vector2 len = new Vector2(hit.point.x, hit.point.y);
+            Debug.DrawRay(transform.position, len, Color.magenta, 3.0f);
+            Debug.Log(hit.collider.gameObject);
+            return hit.collider.gameObject;
+        }
+        return null;
+    }
+    private List<GameObject> GetAllAdjacentGems()
+    {
+        List<GameObject> adjacent = new List<GameObject>();
+        for (int i = 0; i < _adjacentDirs.Length; i++)
+        {
+            adjacent.Add(GetAdjacent(_adjacentDirs[i]));
+        }
+        return adjacent;
+    }
+
+    private List<GameObject> FindMatch(Vector2 dir)
+    {
+        List<GameObject> matching = new List<GameObject>();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir);
+        while (hit.collider != null && hit.collider.GetComponent<Gem>().type == type)
+        {
+            matching.Add(hit.collider.gameObject);
+            hit = Physics2D.Raycast(hit.collider.transform.position, dir);
+        }
+        return matching;
+    }
+    private void ClearMatch(Vector2[] paths)
+    {
+        List<GameObject> matching = new List<GameObject>();
+        for (int i = 0; i < paths.Length; i++)
+        {
+            matching.AddRange(FindMatch(paths[i]));
+        }
+        if (matching.Count >= 2)
+        {
+            for (int i = 0; i < matching.Count; i++)
+            {
+                matching[i].GetComponent<Gem>().Type = GemType.NONE;
+                matching[i].GetComponent<SpriteRenderer>().sprite = null;
+            }
+            _matchFound = true;
+        }
+    }
+    public void ClearAllMatches()
+    {
+        if (transform.GetComponent<SpriteRenderer>().sprite == null || type == GemType.NONE)
+            return;
+
+        ClearMatch(new Vector2[2] { Vector2.left, Vector2.right });
+        ClearMatch(new Vector2[2] { Vector2.up, Vector2.down });
+        if (_matchFound)
+        {
+            transform.GetComponent<SpriteRenderer>().sprite = null;
+            type = GemType.NONE;
+            StopCoroutine(gameInstance.FindNullTiles());
+            StartCoroutine(gameInstance.FindNullTiles());
+            _matchFound = false;
+        }
+    }
+
+    // Setters and Getters
+    public GemType Type
+    {
+        get
+        {
+            return type;
+        }
+        set
+        {
+            type = value;
+        }
+    }
+    public int IType
+    {
+        get
+        {
+            return (int)type;
+        }
+    }
+}
